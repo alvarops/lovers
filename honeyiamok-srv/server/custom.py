3590ffbbe72084acc9c6f44ec37be9adf8687b21
Ignacio Kruss <ignacio.kruss@adtech.com>
2014-10-19 05:02:20 +0100

    added cors headers

--- honeyiamok-srv/server/custom.py
+++ honeyiamok-srv/server/custom.py
@@ -1,4 +1,7 @@
 class custom_middle(object):
 	def process_response(self, request, response):
-		response['Access-Control-Allow-Origin'] = "*"
+		response['Access-Control-Allow-Origin'] = "http://127.0.0.1:9000"
+		response['Access-Control-Allow-Headers'] = "accept, content-type"
+		response['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS"
+		response['Access-Control-Allow-Credentials'] = "true"
 		return response
